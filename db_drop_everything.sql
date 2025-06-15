2e459d10f32327770f425531d258ee98dc9c34cf
kallioaa <aatu.kallio@aalto.fi>
2025-06-15 22:36:36 +0300

    Not needed after code refactoring

--- db_drop_everything.sql
+++
@@ -1,9 +0,0 @@
--- Drop tables and views
-drop table users cascade;
-drop table events cascade;
-drop table todos cascade;
-drop table votes cascade;
-drop table confirmed_score_log cascade;
-
-drop function check_voting_completion;
-drop view events_and_votes_master_view;
